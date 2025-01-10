bbde908ed476dfdea6f195d2a16937805ac1b9f2
avangardewashere <avelpanaligan@gmail.com>
2025-01-10 15:50:42 +0800

    add/reusable-container

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Container } from "./ui/container";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <span>Navbar</span>
+    <div className="border-b">
+      <Container>
+        <div className="relatice px-4 sm:px-6 lg:px-6 flex h-18 items:center">
+          <Link href="" className="ml-4 flex lg:ml-0 gap-x-2"></Link>
+        </div>
+      </Container>
     </div>
   );
 };
