bb90d1ab91ae31acb6fc80109711eb91ea3c96ac
avangardewashere <avelpanaligan@gmail.com>
2025-01-12 21:55:34 +0800

    add/gallery-tab

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+ 'use client'
 
+import { Tab } from "@headlessui/react"
+import Image from "next/image"
+import {Image as ImageType } from "@/types"
 const Gallery = () => {
   return (
     <div>
+      <Tab /> 
+      <Image /> 
       <span>Gallery</span>
     </div>
   )
---
+++ components/Gallery/GalleryTab.tsx
@@ -0,0 +1,38 @@
+import { Image as ImageType } from "@/lib/types";
+import { cn } from "@/lib/utlis";
+import { Tab } from "@headlessui/react";
+import Image from "next/image";
+import React from "react";
+
+interface GalleryTabProps {
+  image: ImageType;
+}
+
+const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
+  return (
+    <Tab className="reltie flex aspect-square cursor-pointer items-center rounded-md bg-white">
+      {({ selected }) => {
+        return (
+          <div>
+            <span className="absolute hfull w-full aspect-square inset-0 overflow-hidden rounded-md">
+              <Image
+                alt=""
+                src={image.url}
+                fill
+                className="object-cover object-center"
+              />
+            </span>
+            <span
+              className={cn(
+                "absolute inset=0 rounded-md ring-2 ring-offset-2",
+                selected ? "ring-black" : "ring-transparent"
+              )}
+            ></span>
+          </div>
+        );
+      }}
+    </Tab>
+  );
+};
+
+export default GalleryTab;
