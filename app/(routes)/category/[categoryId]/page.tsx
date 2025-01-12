aaf72a5021181ebf39680c0942162ee77ddc6a37
avangardewashere <avelpanaligan@gmail.com>
2025-01-13 00:06:05 +0800

    add/fix

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,4 +1,10 @@
+import getColors from "@/actions/getColor";
 import getProducts from "@/actions/getProduct";
+import getSingleCategory from "@/actions/getSingleCatefory";
+import getSizes from "@/actions/getSizes";
+import Billboard from "@/components/ui/Billboard";
+import { Container } from "@/components/ui/container";
+import { Billboard as BTypes } from "@/lib/types";
 
 export const revalidate = 0;
 
@@ -16,14 +22,27 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
   params,
   searchParams,
 }) => {
+
+  const {categoryId} = await params
+  const {colorId,sizeId} = await searchParams 
+  const sizes = await getSizes();
+  const colors = await getColors();
   const products = await getProducts({
-    categoryId: params.categoryId,
-    colorId: searchParams.colorId,
-    sizeId: searchParams.sizeId,
+    categoryId:  categoryId,
+    colorId:  colorId,
+    sizeId:  sizeId,
   });
+  const category = await getSingleCategory(categoryId);
+  console.log("type ",(typeof category.billboard))
+
+  console.log("category - billlboard ",( category.billboard))
+
+ 
   return (
-    <div>
-      <span>category</span>
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard as any} />
+      </Container>
     </div>
   );
 };
