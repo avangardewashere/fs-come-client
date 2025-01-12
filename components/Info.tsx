69f2d1f155943619369f64657126e13e3244b383
avangardewashere <avelpanaligan@gmail.com>
2025-01-12 22:06:32 +0800

    done/info-component

--- components/Info.tsx
+++ components/Info.tsx
@@ -13,6 +13,18 @@ const Info: React.FC<InfoProps> = ({ data }) => {
           <Currency value={data?.price} />
         </p>
       </div>
+      <hr className="my-4" />
+      <div className="flex items-center gap-x-4">
+        <h3 className="font-semibold text-black">Size:</h3>
+        <div>{data?.size?.name}</div>
+      </div>
+      <div className="flex items-center gap-x-4">
+        <h3 className="font-semibold text-black">Color:</h3>
+        <div
+          className="h-6 w-6 rounded-full border border-gray-600"
+          style={{ backgroundColor: data?.color?.value }}
+        ></div>
+      </div>
     </div>
   );
 };
