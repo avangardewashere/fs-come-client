353b7de96c244a7ece73db7c687daf23ac619188
avangardewashere <avelpanaligan@gmail.com>
2025-01-12 21:58:17 +0800

    add/tab-panels

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -1,16 +1,39 @@
- 'use client'
+"use client";
 
-import { Tab } from "@headlessui/react"
-import Image from "next/image"
-import {Image as ImageType } from "@/types"
-const Gallery = () => {
-  return (
-    <div>
-      <Tab /> 
-      <Image /> 
-      <span>Gallery</span>
-    </div>
-  )
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import Image from "next/image";
+import { Image as ImageType } from "@/lib/types";
+import GalleryTab from "./GalleryTab";
+interface GalleryProps {
+  images: ImageType[];
 }
 
-export default Gallery
+const Gallery: React.FC<GalleryProps> = ({ images }) => {
+  return (
+    <TabGroup as="div" className="flex flex-col-reverse">
+      <div className="mx-auto mt-6 hidden w-ful max-w 2xl sm:block lg:max-w-none">
+        <TabList className="grid grid-cols-4 gap-6">
+          {images.map((image) => (
+            <GalleryTab key={image.id} image={image} />
+          ))}
+        </TabList>
+        <TabPanels>
+          {images.map((image) => (
+            <TabPanel key={image.id}>
+              <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+                <Image
+                  fill
+                  src={image.url}
+                  alt="Image"
+                  className="object-cover object-center"
+                />
+              </div>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </div>
+    </TabGroup>
+  );
+};
+
+export default Gallery;
