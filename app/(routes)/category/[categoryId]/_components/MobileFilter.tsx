99778fcb962c4aa5b076965c74c8c445ea7847f9
avangardewashere <avelpanaligan@gmail.com>
2025-01-13 10:52:07 +0800

    add/mobile-filter

---
+++ app/(routes)/category/[categoryId]/_components/MobileFilter.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Button from "@/components/ui/Button";
+import IconButton from "@/components/ui/IconButton";
+import { Color, Size } from "@/lib/types";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Plus, X } from "lucide-react";
+import React, { useState } from "react";
+
+interface MobileFilterProps {
+  sizes: Size[];
+  colors: Color[];
+}
+
+const MobileFilter: React.FC<MobileFilterProps> = ({ sizes, colors }) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+  return (
+    <>
+      <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
+        Filters
+        <Plus size={20}></Plus>
+      </Button>
+
+      <Dialog
+        open={open}
+        as="div"
+        className="relatice z-48 lg:hidden"
+        onClose={onClose}
+      >
+        {/* background */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        {/* Dialog Position */}
+        <div className="fixed inset-0 z-40 flex">
+          <DialogPanel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 b-6 shadow-xl">
+            {/* Close Button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<X size={15} />} onClick={onClose} />
+            </div>
+
+            {/* Render the Filters */}
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Color" data={colors} />
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default MobileFilter;
--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -6,6 +6,8 @@ import Billboard from "@/components/ui/Billboard";
 import { Container } from "@/components/ui/container";
 import { Billboard as BTypes } from "@/lib/types";
 import Filter from "./_components/Filter";
+import NoResult from "@/components/ui/NoResult";
+import ProductCard from "@/components/ui/ProductCard";
 
 export const revalidate = 0;
 
@@ -43,8 +45,18 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category?.billboard as any} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           {/* mobile filters */}
+          <MobileFilters sizes={sizes} colors={colors} />
           <div className="hidden lg:block">
             <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+            <Filter valueKey="colorId" name="Color" data={colors} />
+          </div>
+          <div className="mt-6 lg:col-span-4 lg:mt-8">
+            {products.length === 0 && <NoResult />}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {products.map((item) => (
+                <ProductCard key={item.id} data={item} />
+              ))}
+            </div>
           </div>
         </div>
       </Container>
